3e094a83052291851671e12701ac3a556bcee61a
Aryansh Mahato <contact@aryansh.dev>
2023-07-30 18:34:32 +0530

    test(health): add vitest GH-1

--- src/features/health/health.controller.spec.ts
+++ src/features/health/health.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthController } from './health.controller';
+import { describe, beforeEach, it, expect } from 'vitest';
 
 describe('HealthController', () => {
   let controller: HealthController;
