ddd550fb210e29f0f43f75b3c7959d0c8379efb4
Aryansh Mahato <contact@aryansh.dev>
2023-07-30 19:06:29 +0530

    feat(interceptor): remove validation pipe GH-1

--- src/core.module.ts
+++ src/core.module.ts
@@ -3,20 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './features/health/health.module';
 import { PassportModule } from '@nestjs/passport';
-import { APP_PIPE } from '@nestjs/core';
-import { NestByteValidationPipe } from './pipes/nest-byte-validation.pipe';
 
 @Global()
 @Module({
   imports: [HealthModule, PassportModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_PIPE,
-      useClass: NestByteValidationPipe,
-    },
-  ],
+  providers: [AppService],
   exports: [],
 })
 export class CoreModule {}
