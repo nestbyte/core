efb9775abd038212d2a82e28e66efc370a0b64dd
Aryansh Mahato <contact@aryansh.dev>
2023-08-01 13:34:28 +0530

    ci: fix semantic release GH-1

--- release.config.js
+++ release.config.js
@@ -3,14 +3,15 @@ const config = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/github',
     [
       '@semantic-release/git',
       {
-        assets: ['dist/*'],
-        message: `chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}`,
+        assets: ['dist/**/*', 'package.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
+    '@semantic-release/github',
   ],
 };
 
